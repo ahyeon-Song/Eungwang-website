f38877e0b1ee0f691af129ed779e563d93456da6
Ahyeon Song <sahjump@gmail.com>
2022-03-07 17:54:24 +0900

    second commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Preview img
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".preview", {
+    scrollTrigger: {
+      trigger: '.preview',
+      start: '+=20',
+      end: '+=150',
+      scrub: 1,
+      pin: true,
+      pinSpacing: false,
+      toggleActions: 'play none none none',
+    },
+    scale: 2,
+    opacity: 0
+  }); 
+
+
+
+
+    
+
+  
