f7c118e424ed86e36ec776b9e9ac9cd47679c09f
Ahyeon Song <sahjump@gmail.com>
2022-03-10 17:53:50 +0900

    second commit

--- script.js
+++ script.js
@@ -26,6 +26,7 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+// 로고
 gsap.to("#intro-logo" , {
     zoom: 0.09,
     duration: 1.2,
@@ -41,22 +42,43 @@ gsap.to("#intro-text" , {
 })
 
 gsap.to("img-1" , {
-  x: 50,
-  y: -80,
+  x: 230,
+  y: -100,
+  duration: 2,
+  scrolltrigger: "img-1"
+})
+
+gsap.to("img-2" , {
+  x: 40,
+  y: -100,
   duration: 2,
   scrolltrigger: "img-1"
 })
 
 gsap.to("img-3" , {
+  x: -100,
   y: -100,
   duration: 2,
   scrolltrigger: "img-1"
 })
 
 gsap.to("img-4" , {
-  x: -50,
-  y: -80,
+  x: -230,
+  y: -100,
   duration: 2,
   scrolltrigger: "img-1"
 })
-  
+
+// 은광 비디오
+gsap.to(".about-video", {
+    scrollTrigger: {
+      trigger: '.summary',
+      start: 'bottom +=80',
+      end: '+=3',
+      scrub: 0.2,
+      pin: 'true',
+      pinSpacing: false,
+      toggleActions: 'play none none none',
+    },
+    scale:0.9,
+  }); 
